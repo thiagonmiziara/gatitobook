d859d094f62eb8203378c0803fa846b21a09774d
thiago miziara <thiagonmiziara@gmail.com>
2021-06-11 11:22:04 -0300

    add primeiro component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  exports: [HomeComponent],
 })
 export class HomeModule { }
